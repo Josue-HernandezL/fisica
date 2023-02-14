f64fa097679a8c82f03b81ce07b29ab7898d9792
Josue-HernandezL <jh6466011@gmail.com>
2023-02-13 20:42:26 -0600

    terminado

--- calculador de peso/inde.js
+++ calculador de peso/inde.js
@@ -3,11 +3,12 @@ var contrapeso = document.getElementById("contraPeso");
 var barra = document.getElementById("barra");
 var pesoBarra = document.getElementById("pesoBarra");
 var calculo = document.getElementById("calculoFisica");
+var pesoCesta = document.getElementById("pesoCesta");
 boton.addEventListener("click", function(){
     calculo.innerHTML="";
     var mediaBarra = barra.value/4;
     var barraDistancia = mediaBarra *3;
    for (i = 0; i <= barraDistancia; i++) {
-        calculo.innerHTML += `<p class="resultado">${i} cm:</p><p class="resultado">${( (pesoBarra.value * mediaBarra) + ( contrapeso.value * i) - (20 * mediaBarra) ) / mediaBarra}</p>`;      /* peso.value * i + "<br>" */
+        calculo.innerHTML += `<p class="resultado">${i} cm:</p><p class="resultado color1">${( (pesoBarra.value * mediaBarra) + ( contrapeso.value * i) - (pesoCesta.value * mediaBarra) ) / mediaBarra} g</p>`;      /* peso.value * i + "<br>" */
    }
 });
